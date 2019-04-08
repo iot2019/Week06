918622ad336812f24fcf2dc1592fc826c6327006
jinsook64 <putzmunter7@gmail.com>
2019-04-08 23:36:30 +0900

    틀린내용 정정(풀업저항 조도센서)

--- 03_analogCDS/03_analogCDS.ino
+++ 03_analogCDS/03_analogCDS.ino
@@ -6,10 +6,11 @@ void setup() {
 
 void loop() {
   int val = analogRead(A0); //조도센서에 따른 값을 읽어드림
-  val = map(val, 0,1023,0,255);//값을 0~255값으로 변환
-  val = 255 - val;  //어두워지면 LED를 밝게하기 위해 값을
-                    //반전시킴
+    //PULL-UP 저항의 경우
+    //조도저항값 : 0(주위가 환할 수록), 1023(주위가 어두울 수록)
+    //LED 값 : 0 (주위가 환하면 어둡게 켜기), 255(주위가 어두우면 환하게 켜기) 
+  val = map(val, 0, 1023, 0, 255);
+  Serial.println(val);  //화면 출력
   analogWrite(ledPin, val); //PWM이 가능한 핀에 출력
   delay(300);
-  Serial.println(val);  //시리얼모니터에 값 확인
 }
